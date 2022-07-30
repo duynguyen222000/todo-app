871ae93fa8a56cd4f17b1dfd9eca7a8f8c7ebe21
duynguyen222000 <nguyenducduy02022000@gmail.com>
2022-07-30 13:33:59 +0700

    13:33 30/07

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,11 +4,108 @@ import { Container } from "../styled-components/Container";
 import { ToDoListLightTheme } from "../theme/ToDoListLightTheme";
 import { ToDoListDarkTheme } from "../theme/ToDoListDarkTheme";
 import { ToDoListPrimaryTheme } from "../theme/ToDoListPrimaryTheme";
+import { Dropdown } from "../styled-components/Dropdown";
+import {
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  Heading5,
+} from "../styled-components/Heading";
+import { TextField } from "../styled-components/TextField";
+import { Button } from "../styled-components/Button";
+import {
+  AiOutlinePlus,
+  AiOutlineUpload,
+  AiFillEdit,
+  AiOutlineCheck,
+  AiOutlineClose,
+} from "react-icons/ai";
+import { Table, Thead, Tr, Th } from "../styled-components/Table";
 class TodoList extends Component {
   render() {
     return (
-      <ThemeProvider theme={ToDoListPrimaryTheme}>
-        <Container>123</Container>
+      <ThemeProvider theme={ToDoListDarkTheme}>
+        <Container>
+          <Dropdown>
+            <option value="">Primary Theme</option>
+            <option value="">Dark Theme</option>
+            <option value="">Light Theme</option>
+          </Dropdown>
+          <Heading1 className="py-2 my-2">To do app</Heading1>
+          <div className="my-2 py-2 flex">
+            <TextField
+              span={"flex grow items-center"}
+              label={"Task name"}
+              className="ml-2 grow"
+              onChange={(e) => {
+                console.log(e.target.value);
+              }}
+            />
+            <Button className="ml-2">
+              <AiOutlinePlus style={{ display: "inline-block" }} />
+              Add task
+            </Button>
+            <Button className="ml-2">
+              <AiOutlineUpload style={{ display: "inline-block" }} />
+              Update task
+            </Button>
+          </div>
+          <Heading3>Task to do</Heading3>
+          <Table>
+            <Thead>
+              <Tr>
+                <Th className="text-start">Task name</Th>
+                <Th className="text-right">
+                  <Button>
+                    <AiFillEdit />
+                  </Button>
+                  <Button>
+                    <AiOutlineCheck />
+                  </Button>
+                  <Button>
+                    <AiOutlineClose />
+                  </Button>
+                </Th>
+              </Tr>
+              <Tr>
+                <Th className="text-start">Task name</Th>
+                <Th className="text-right">
+                  <Button>
+                    <AiFillEdit />
+                  </Button>
+                  <Button>
+                    <AiOutlineCheck />
+                  </Button>
+                  <Button>
+                    <AiOutlineClose />
+                  </Button>
+                </Th>
+              </Tr>
+            </Thead>
+          </Table>
+          <Heading3 className="py-2">Task complete</Heading3>
+          <Table>
+            <Thead>
+              <Tr>
+                <Th className="text-start">Task name</Th>
+                <Th className="text-right">
+                  <Button>
+                    <AiOutlineClose />
+                  </Button>
+                </Th>
+              </Tr>
+              <Tr>
+                <Th className="text-start">Task name</Th>
+                <Th className="text-right">
+                  <Button>
+                    <AiOutlineClose />
+                  </Button>
+                </Th>
+              </Tr>
+            </Thead>
+          </Table>
+        </Container>
       </ThemeProvider>
     );
   }
--- src/styled-components/TextField.js
+++ src/styled-components/TextField.js
@@ -1,29 +1,26 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 const Input = styled.input`
-    border: 1px solid ${props => props.theme.color};
-    min-height:35px;
-    height:35px;
-    font-size:17px;
-    width:auto;
-    display:initial;
-`
+  border: 1px solid ${(props) => props.theme.color};
+  min-height: 35px;
+  height: 35px;
+  font-size: 17px;
+  display: initial;
+  color: #000;
+`;
 
 const Label = styled.span`
-    color:${props => props.theme.color};
-    width:auto;
+  color: ${(props) => props.theme.color};
+  width: auto;
+`;
 
-`
-
-
-export const TextField = ({ label, ...props }) => {
-    return <span>
-        <Label>
-            {label}
-        </Label>
-        <Input {...props} />
+export const TextField = ({ span, label, ...props }) => {
+  return (
+    <span className={span}>
+      <Label>{label}</Label>
+      <Input {...props} />
     </span>
-}
+  );
+};
